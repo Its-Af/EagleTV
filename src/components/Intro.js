17a1e20579a09417d7e3459de17b4b8968affc94
Afshin Hamidi <afshin.hamidi9@gmail.com>
2024-04-25 13:09:59 -0400

    Update Hero, Intro, Services components

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../images/Web-developer.svg';
+import TechsMountingTV_2 from '../images/TechsMountingTV_2.jpeg';
 import { Link } from 'react-router-dom';
 
 const Intro = () => {
@@ -9,17 +9,22 @@ const Intro = () => {
 
                     <div className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left" data-aos="fade-up">
                         <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
-                            <img alt="card img" className="rounded-t float-right" src={img} />
+                            <img alt="card img" className="rounded-t float-right" src={TechsMountingTV_2} />
                         </div>
                         <div className="flex-col my-4 text-center lg:text-left lg:my-0 lg:justify-end w-full lg:w-1/2 px-8" data-aos="zoom-in" data-aos-delay="500">
                             
-                            <h3 className="text-3xl  text-blue-900 font-bold">We develop high quality bespoke web and mobile applications for organizations, institutions and SMEs</h3>
+                            <h3 className="text-3xl  text-blue-900 font-bold">Our mission is to provide exceptional service and high-quality installations for all of our clients.</h3>
+                            {/* <h3 className="text-3xl  text-blue-900 font-bold"> We strive to exceed customer expectations and ensure that every installation is completed with the utmost attention to detail and safety.</h3> */}
                             <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>Our team is well vast in software development and is ready to help develop the applications of your choice.</p>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'>We understand the importance of having a properly mounted TV for both aesthetics and functionality. Whether it’s for a home theater setup or a business display, we are committed to delivering the best possible result.</p>
                             </div>
                             
                             <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>We take responsibility for building custom software solutions that caters for automation of your business processes and improve efficiency.</p>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'>Our team of experienced technicians is trained and certified to handle any installation, no matter the size or complexity.</p>
+                            </div>
+
+                            <div>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'>We guarantee safe installations with 100% lifetime "No Fall" warranty.</p>
                             </div>
                             <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-2 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
                                 Contact us
--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import NavLinks from '../Navbar/NavLinks';
 import { HashLink } from 'react-router-hash-link';
+import companyLogo from '../../images/EagleTVLogo.png';
+
 
 
 const NavBar = () => {
@@ -21,9 +23,9 @@ const NavBar = () => {
 
     return (
         <nav className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 ${!top && 'bg-white shadow-lg'}`}>
-            <div className="flex flex-row justify-between items-center py-2">
+            <div className="flex flex-row justify-between items-center py-2" style={{height:"75px"}}>
                 <div className="flex flex-row justify-center md:px-12 md:mx-12 items-center text-center font-semibold">
-                    <HashLink smooth to="/#hero"><h1 className="font-extrabold text-4xl text-blue-900">mld</h1></HashLink>
+                    <HashLink smooth to="/#hero"><img alt="Company Logo" src={companyLogo} className="navLogo font-extrabold text-4xl text-blue-900"></img></HashLink>
                     
                 </div>
                 <div className="group flex flex-col items-center">
